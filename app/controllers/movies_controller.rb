bfce21f37780089b650562b27af3b70dc6e99064
Tamada Shutaro <s.tamada@raksul.com>
2026-04-16 11:10:28 +0900

    station11: 予約不可の座席を表示

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -53,43 +53,53 @@ class MoviesController < ApplicationController
       return 
     end
 
+    # dataパラメータのフォーマットチェック
     @date_input = params[:date] # 日付を受け取る(YYYY-MM-DD HH:MM)
     unless @date_input&.match?(/\A\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\z/) 
-      # redirect_to movies_path,  alert: "日付がない"
+      redirect_to movies_path, alert: "スケジュールが見つかりませんでした。"
+      return 
     end
 
-    @movie =  Movie.find(params[:id])
-    
-
     @date = Time.parse(@date_input).strftime("%Y-%m-%d")
-
+    @movie =  Movie.find(params[:id])
     @schedule = Schedule.find_by(id: params[:schedule_id])
     if @schedule.nil?
       redirect_to movies_path, alert: "スケジュールが見つかりませんでした。"
     else
       @schedule_id = @schedule.id
-    end
-
-    #@schedule = Schedule.find(params[:schedule_id])
-    
+    end    
 
     @sheets = Sheet.all
+    @reservations = Reservation.where(date: @date, schedule_id:@schedule_id)
 
     # 配列に変換
     @sheets_array = [] # 最終結果
     $tmp_array = [] # 一時保存
+    @sheets_has_reservation = []
+    $tmp_sheets_has_reservation = []
     
     @sheets.each_with_index do |sheet, i|
       if i > 0 && @sheets[i - 1]["row"] != @sheets[i]["row"]
         # rowが変わるタイミングで$tmp_arrayをsheets_arrayに挿入
         @sheets_array.push($tmp_array)
         $tmp_array = []
+        @sheets_has_reservation.push($tmp_sheets_has_reservation)
+        $tmp_sheets_has_reservation = []
       end
       $tmp_array.push(@sheets[i])
+
+      if @reservations.any? { |reservation| reservation[:sheet_id] == @sheets[i].id }
+        $tmp_sheets_has_reservation.push(true)
+      else
+        $tmp_sheets_has_reservation.push(false)
+      end
+
+      
     end
 
     if $tmp_array.length != 0
       @sheets_array.push($tmp_array)
+      @sheets_has_reservation.push($tmp_sheets_has_reservation)
     end
   end
 
